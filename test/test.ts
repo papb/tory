fabb1c7e7e1d93e65d2eb3533bb67daf080962d1
papb <papb1996@gmail.com>
2020-01-24 15:50:19 -0300

    clean test

--- test/test.ts
+++ test/test.ts
@@ -1,15 +1,12 @@
 import test from 'ava';
 import tory from '../source';
-import jetpack = require('fs-jetpack');
 
-test('title', t => {
+test('describeFolder', t => {
 	// T.throws(() => {
 	// 	(tory as any)(123);
 	// }, 'Expected a string, got number');
 
 	const result = tory.describeFolder('.');
-
-	jetpack.write('argh.json', JSON.stringify(result, null, '\t'));
 
 	t.true(result.files.some(file => file.name === 'package.json'));
 	t.true(result.folders.some(folder => folder.name === 'source'));
