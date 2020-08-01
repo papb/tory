5a6451ebacfc270f1f485aa1632842ef6eeef41d
papb <papb1996@gmail.com>
2020-08-01 18:32:40 -0300

    Refactor `ToryFiler` interface

--- source/helpers/tory-filer.ts
+++ source/helpers/tory-filer.ts
@@ -2,6 +2,6 @@ export interface ToryFiler {
 	readonly absolutePath: string;
 	readonly name: string;
 	readonly type: 'file' | 'folder';
-	getHash(): string;
-	getSize(): number;
+	getHash: () => string;
+	getSize: () => number;
 }
